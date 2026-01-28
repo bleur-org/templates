46a6ddc0be8f0f955cf42517005c5c211d44f3be
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2026-01-29 03:03:26 +0500

    feat(python) finish/add first draft of python template

---
+++ python/docs/source/conf.py
@@ -0,0 +1,11 @@
+project = "@name@"
+copyright = "2026, @author@"  # noqa: A001
+author = "@author@"
+
+extensions: list[str] = ["sphinx_copybutton"]
+
+templates_path = ["_templates"]
+exclude_patterns: list[str] = []
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
